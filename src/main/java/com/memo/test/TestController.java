8bc501799fdbb2cbeb2ba7dacc7db98e2656605d
test <test@test.com>
2024-01-12 16:02:53 +0900

    초기 환경 세팅

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,52 @@
+package com.memo.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.memo.post.mapper.PostMapper;
+
+@Controller
+public class TestController {
+
+	@Autowired
+	private PostMapper postMapper;
+	
+	@ResponseBody
+	@GetMapping("/test1")
+	public String test1() {
+		return "Hello World!";
+	}
+	
+	@GetMapping("/test2")
+	@ResponseBody
+	public Map<String , Object> test2() {
+		
+		Map<String, Object> result = new HashMap<>();
+		result.put("aaa", 111);
+		result.put("bbb", 222);
+		result.put("ccc", 333);
+		
+		return result;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test4")
+	// url : http://localhost/test4
+	public List<Map<String, Object>> test4() {
+		
+		return postMapper.selectPostList();
+		
+	}
+	
+}
